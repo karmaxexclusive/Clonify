259de4056f9d63938de22fcf5df5f8efd161bf25
Dev <190914293+karmaxexclusive@users.noreply.github.com>
2025-06-07 13:14:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -59,7 +59,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/karmaxexclusive/Clonify",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )
